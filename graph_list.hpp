f0a91ab63e529d2c5c9be88767dd6b6473027cc8
gius-toma <giuseppetomarchioschool@gmail.com>
2022-06-14 09:16:10 +0200

    Implemented Graph_List::add_edge()

--- graph_list.hpp
+++ graph_list.hpp
@@ -5,25 +5,32 @@
 template <typename T>
 class Graph_List {
     List<Graph_Vertex<T>> m_vertices;
+
     std::size_t n_vertex;
+    bool is_oriented;
 
 public:
-    Graph_List() : n_vertex(0) {}
+    Graph_List(bool is_oriented=true) : n_vertex(0), is_oriented(is_oriented) {}
 
     bool add_vertex(T const& data) {
         return add_vertex(new Vertex<T>(data));
     }
 
-    bool add_vertex(Vertex<T>* vertex) {
-        if(this->n_vertex == this->max_vertex)
-            return false;
-        
+    bool add_vertex(Vertex<T>* vertex) {        
         vertices[n_vertex++] = vertex;
         return true;
     }
 
     void add_edge(T const& data0, T const& data1) {
+        Node<Graph_Vertex<T>>* node0 = search(data0);
+        Node<Graph_Vertex<T>>* node1 = search(data1);
 
+        if (node0 && node1) {
+            node0->get_data().insert(data1);
+            if (!this->is_oriented) {
+                node1->get_data().insert(data0);
+            }
+        }
     }
 
     Node<Graph_Vertex<T>>* search(T const& data) const {
